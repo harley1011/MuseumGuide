c97dc6ed90b0aa51c21721ad50ab2c8d3cf0845e
Harley <harley.1011@gmail.com>
2016-01-27 09:00:35 -0500

    Working on using canvas to load the map image into

--- www/js/directives/mapDirective.js
+++ www/js/directives/mapDirective.js
@@ -7,6 +7,16 @@ angular.module('directives')
                 console.log(element.find('canvas'));
                 console.log(element.find('div')[0].offsetHeight);
                // element.find('canvas').offsetHeight = element.find('div')[0].offsetHeight;
+                window.onload = function() {
+                    var canvas = document.getElementById("myCanvas"),
+                        ctx = canvas.getContext("2d");
+                    console.log(canvas.width);
+                    console.log(canvas.height)
+                    var image = new Image();
+                    image.src = "img/one.png";
+                    console.log(image);
+                    ctx.drawImage(image, 0, 0, 200,200);
+                }
             }
         }
     });
