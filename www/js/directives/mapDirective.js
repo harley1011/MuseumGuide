bea46b7b000e292537848c8a900ed5894ee8d48d
harley1011 <harley.1011@gmail.com>
2016-01-27 11:35:56 -0500

    Working on the map directive

--- www/js/directives/mapDirective.js
+++ www/js/directives/mapDirective.js
@@ -6,17 +6,37 @@ angular.module('directives')
             link: function (scope, element, attrs) {
                 console.log(element.find('canvas'));
                 console.log(element.find('div')[0].offsetHeight);
-               // element.find('canvas').offsetHeight = element.find('div')[0].offsetHeight;
-                window.onload = function() {
-                    var canvas = document.getElementById("myCanvas"),
-                        ctx = canvas.getContext("2d");
-                    console.log(canvas.width);
-                    console.log(canvas.height)
-                    var image = new Image();
-                    image.src = "img/one.png";
-                    console.log(image);
-                    ctx.drawImage(image, 0, 0, 200,200);
+                // element.find('canvas').offsetHeight = element.find('div')[0].offsetHeight;
+
+                var canvas = document.getElementById("myCanvas"),
+                    ctx = canvas.getContext("2d");
+                //canvas.width = element.find('div')[0].offsetWidth;   /// use integer values
+                canvas.height = element.find('div')[0].offsetHeight;
+                var img = new Image();
+                img.src = "img/one.png";
+                img.onload = function(){
+                    console.log("image loaded");
+                    scope.draw();
+                }
+                scope.draw = function() {
+                    canvas.width = canvas.height * (img.width / img.height);
+
+                    /// step 1 - resize to 50%
+                    var oc = document.createElement('canvas'),
+                        octx = oc.getContext('2d');
+
+                    oc.width = img.width * 0.5;
+                    oc.height = img.height * 0.5;
+                    octx.drawImage(img, 0, 0, oc.width, oc.height);
+
+                    /// step 2 - resize 50% of step 1
+                    octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
+
+                    /// step 3, resize to final size
+                    ctx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5,
+                        0, 0, canvas.width, canvas.height);
                 }
             }
+
         }
     });
