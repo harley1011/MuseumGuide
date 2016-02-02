1c7bcd7e707253600b9d21e57444219ceeee43f8
Lutherson Mendes <lucson312@gmail.com>
2016-02-01 19:52:43 -0500

    BUILD ALLOW TO FAIL test.

--- tests/controllers/mapCtrl.test.js
+++ tests/controllers/mapCtrl.test.js
@@ -1,5 +1,5 @@
 describe('Testing Jasmin on map Ctrl', function() {
     it('should spec with an expectation', function() {
-        expect(true).toBe(true);
+        expect(true).toBe(false);
     });
 });
