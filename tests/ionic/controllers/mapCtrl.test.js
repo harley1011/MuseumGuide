0fbba67a7f981980c276aa8a355e07dbd34e546a
davidbastien5 <david.bastien5@gmail.com>
2016-02-07 16:08:12 -0500

    Failed attempt at writing tests

---
+++ tests/ionic/controllers/mapCtrl.test.js
@@ -0,0 +1,34 @@
+/*describe('controllers', function () {
+    var $rootScope,
+        $compile;
+
+    beforeEach(module('controllers')); // load controllers module from project
+
+    beforeEach(module('services'));
+
+    beforeEach(module('my.templates'));
+
+    beforeEach(inject(function(_$controller_, _iBeaconSrvc_, _storyLinePathSrvc_, _mapDataSrvc_){
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
+        $service = _$service_;
+        iBeaconSrvc = _iBeaconSrvc_;
+        storyLinePathSrvc = _storyLinePathSrvc_;
+        mapDataSrvc = _mapDataSrvc_;
+    }));
+
+    describe('map controller test', function () {
+        it('SHOULD TEST', function () {
+            var $scope = {};
+            var controller = $controller('mapCtrl', {
+                $scope: $scope,
+                storyLinePathSrvc: storyLinePathSrvc,
+                iBeaconSrvc: iBeaconSrvc,
+                mapDataSrvc: mapDataSrvc
+            });
+            $scope.grade();
+            expect($scope.currentLevel.number).toEqual(1);
+        });
+    });
+
+});*/
