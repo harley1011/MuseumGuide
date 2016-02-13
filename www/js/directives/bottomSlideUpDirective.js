d6c77338d64cf26be43c57f6b5790790af215cf3
harley1011 <harley.1011@gmail.com>
2016-02-12 19:00:55 -0500

    SOEN-62 #comment Adding scroll to setting sub menu list

--- www/js/directives/bottomSlideUpDirective.js
+++ www/js/directives/bottomSlideUpDirective.js
@@ -7,14 +7,13 @@ angular.module('directives')
             link: function (scope, element, attrs, ctrls) {
                 ctrls[0].subMenuActive = false;
 
-
                 ctrls[0].backToMenu = function() {
                     ctrls[0].subMenuActive = false;
                     ctrls[0].changeIconForTab('ion-qr-scanner');
                     angular.element(element[0].querySelector('.sub-menu-list')).removeClass('slide-sub-menu-list');
                     console.log('back to menu');
                 }   
-                scope.choseStoryLine = function() {
+                scope.choseStoryLines = function() {
                     ctrls[0].subMenuActive = true;
                     ctrls[0].changeIconForTab('ion-log-out');
                     angular.element(element[0].querySelector('.sub-menu-list')).addClass('slide-sub-menu-list');
