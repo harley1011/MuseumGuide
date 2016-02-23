35ad6cffba2ae00800ac2f3a711e0e69c155f55f
p-hebert <philippe.hebert.qc@gmail.com>
2016-02-23 00:19:02 -0500

    Implemented Floor and FloorPlan types. Currently untested and not integrated in the program flow.

---
+++ www/js/types/FloorPlan.proto.js
@@ -0,0 +1,39 @@
+var FloorPlan = (function (){
+
+  var privateData = new WeakMap();
+
+  //Constructor Function
+  function FloorPlan(raw) {
+    var privateMembers = {
+      url: raw.url,
+      width: raw.width,
+      height: raw.height,
+    };
+    privateData.set(this, privateMembers);
+  }
+
+  FloorPlan.prototype.constructor = FloorPlan;
+
+  FloorPlan.prototype.getNumber = function(){
+    return privateData.get(this).url;
+  };
+
+  FloorPlan.prototype.getDimensions = function(){
+    var floorPlan = privateData.get(this);
+    return {width: floorPlan.width, height: floorPlan.height};
+  };
+
+  FloorPlan.prototype.getWidth = function(){
+    return privateData.get(this).width;
+  };
+
+  FloorPlan.prototype.getHeight = function(){
+    return privateData.get(this).height;
+  };
+
+  FloorPlan.prototype.destructor = function(){
+    privateData.delete(this);
+  };
+
+  return FloorPlan;
+}());
