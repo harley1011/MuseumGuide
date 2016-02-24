819496ebb4cb2c0c0b57565b1d7f680b5c25aad3
harley1011 <harley.1011@gmail.com>
2016-02-24 15:21:45 -0500

    SOEN-101 #comment Changing language now brings the user back to the settings page

--- www/js/controllers/langCtrl.js
+++ www/js/controllers/langCtrl.js
@@ -1,15 +1,15 @@
 angular.module('controllers')
-.controller('langCtrl', function($scope, $state, $translate) {
+.controller('langCtrl', function($scope, $state, $translate, $ionicHistory) {
     $scope.english = function () {
         console.log("Speaking English");
 		$scope.changeLanguage("en");
-        $state.go('tab.level');
+        $ionicHistory.goBack(-1);
     };
 
 	$scope.french = function () {
 		console.log("Speaking French");
 		$scope.changeLanguage("fr");
-		$state.go('tab.level');
+        $ionicHistory.goBack(-1);
 	};
 
 
