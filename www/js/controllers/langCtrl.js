a29d5c538ec2e635fb07ad8ac3312632d5dd9cbe
harley1011 <harley.1011@gmail.com>
2016-02-25 13:49:50 -0500

    Fixing start menu not going to the next state after the button is clicked

--- www/js/controllers/langCtrl.js
+++ www/js/controllers/langCtrl.js
@@ -1,17 +1,24 @@
 angular.module('controllers')
 .controller('langCtrl', function($scope, $state, $translate, $ionicHistory) {
     $scope.english = function () {
-        console.log("Speaking English");
 		$scope.changeLanguage("en");
-        $ionicHistory.goBack(-1);
+        $state.go('tab.level');
     };
 
 	$scope.french = function () {
-		console.log("Speaking French");
 		$scope.changeLanguage("fr");
-        $ionicHistory.goBack(-1);
+        $state.go('tab.level');
 	};
 
+    $scope.changeToEnglish = function () {
+        $scope.changeLanguage("en");
+        $ionicHistory.goBack(-1);
+    };
+
+    $scope.changeToFrench = function () {
+        $scope.changeLanguage("fr");
+        $ionicHistory.goBack(-1);
+    };
 
   $scope.changeLanguage = function (langKey) {
     $translate.use(langKey);
