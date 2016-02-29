0a9617b453f1d0cd6e023d9bc05d92e97381668f
Lutherson Mendes <lucson312@gmail.com>
2016-02-28 21:28:36 -0500

    added second header to play media. Next step, add audio

---
+++ www/js/directives/twoSubHeaderFix.js
@@ -0,0 +1,35 @@
+/* http://ionicinaction.com/blog/ionic-custom-double-subheaders/ */
+angular.module('directives')
+  .directive('positionBarsAndContent', function($timeout) {
+
+    return {
+
+      restrict: 'AC',
+
+      link: function(scope, element) {
+
+        var offsetTop = 44;
+
+        // Get the parent node of the ion-content
+        var parent = angular.element(element[0].parentNode);
+
+        // Get all the headers in this parent
+        var headers = parent[0].getElementsByClassName('bar');
+
+        // Iterate through all the headers
+        for (x = 0; x < headers.length; x++) {
+          // If this is not the main header or nav-bar, adjust its position to be below the previous header
+          if (x > 0) {
+            headers[x].style.top = offsetTop + 'px';
+          }
+
+          // Add up the heights of all the header bars
+          offsetTop = offsetTop + headers[x].offsetHeight;
+        }
+
+        // Position the ion-content element directly below all the headers
+        element[0].style.top = offsetTop + 'px';
+
+      }
+    };
+  });
