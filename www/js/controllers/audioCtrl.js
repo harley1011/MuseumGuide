2cd7af72acca1628377eac3b2db5e2db435a205e
Lutherson Mendes <lucson312@gmail.com>
2016-02-28 22:23:00 -0500

    Fixed audio issue on android by modif the path

--- www/js/controllers/audioCtrl.js
+++ www/js/controllers/audioCtrl.js
@@ -1,4 +1,4 @@
 angular.module('controllers')
 .controller("audioCtrl",function($scope,ngAudio){
-    $scope.audio = ngAudio.load("../audio/bird.mp3"); // returns NgAudioObject
+    $scope.audio = ngAudio.load("../www/audio/bird.mp3"); // returns NgAudioObject
 })
