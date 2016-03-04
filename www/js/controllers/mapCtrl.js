e2b40d46771381c63d54647b6531c435c36327dc
Lutherson Mendes <lucson312@gmail.com>
2016-03-03 23:28:59 -0500

    SOEN-105 #comment Directive now ready PP: @tamy

--- www/js/controllers/mapCtrl.js
+++ www/js/controllers/mapCtrl.js
@@ -2,10 +2,6 @@ angular.module('controllers')
 	.controller('mapCtrl', function ($scope, $state, $translatePartialLoader, iBeaconSrvc, storyLinePathSrvc, JSONFactorySrvc, $interval, $ionicPopup) {
 		var mapData = {};
 
-        $scope.getDetails = function () {
-            $state.go('tab.details');
-        };
-
 		(function init() {
 			$translatePartialLoader.addPart('map');
 
@@ -22,7 +18,10 @@ angular.module('controllers')
 				prepareData(mapData);
 			};
 
-
+			$scope.getDetails = function () {
+	            $state.go('tab.details');
+	        };
+			
 			$scope.$on('storyLineChosen', function (event, storyLine) {
 				$scope.storyLineID = storyLine.getUUID();
 				$scope.alreadyPopup = [];
